0481bb39698d9928208d0bcc557e53aae62bc211
Bhargav Parekh <42684274+nambvp03@users.noreply.github.com>
2018-11-28 14:31:19 -0800

    Advance UI

---
+++ advance_ui/app.js
@@ -0,0 +1,19 @@
+var searchApp = angular.module('searchApp', []);
+
+  searchApp.controller('SearchController', ['$scope','$http',function($scope, $http) {
+    var searchQuery = this;
+
+    searchQuery.search = function() {
+	//console.log(searchQuery.queryString);
+    	$http({ method: 'GET', url: 'http://localhost:9200/imdb/_search?q=*'+searchQuery.queryString+'*' })
+	//$http({ method: 'GET', url: 'test.json' })
+      	.then(function (response) {
+            //console.log(response.data);
+            return searchQuery.results = response.data;
+	    //return searchQuery.results = [{"took":1,"timed_out":false,"_shards":{"total":5,"successful":5,"skipped":0,"failed":0},"hits":{"total":0,"max_score":null,"hits":[]}}]
+      	}, function (response) {
+
+      	}
+   	);
+    };
+  }]);

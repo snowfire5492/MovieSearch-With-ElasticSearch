72ea94310be3c507bf2ccd5fd960e7d1bd311d4a
mathagoris <matt.dohlen@gmail.com>
2018-11-29 16:57:00 -0800

    Logging for clicks, queries, and session duration

--- advance_ui/app.js
+++ advance_ui/app.js
@@ -2,9 +2,33 @@ var searchApp = angular.module('searchApp', []);
 
   searchApp.controller('SearchController', ['$scope','$http',function($scope, $http) {
     var searchQuery = this;
+    searchQuery.sessionInfo = {
+      sessionID:sessionID(),
+      queries:new Array,
+      total_queries:0,
+      clicks:new Array,
+      total_clicks:0,
+      start:new Date().toString()
+    };
+
+    console.log("New session (sessionID: " + searchQuery.sessionInfo.sessionID +
+      ") started at " + searchQuery.sessionInfo.start);
+
+    function sessionID() {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    };
 
     searchQuery.search = function() {
-	//console.log(searchQuery.queryString);
+      var now = new Date();
+      var logEntry = "'" + searchQuery.queryString + "' queried at " + now.toString();
+      console.log(logEntry);
+      searchQuery.sessionInfo.queries.push(logEntry);
+      searchQuery.sessionInfo.total_queries += 1;
     	$http({ method: 'GET', url: 'http://localhost:9200/imdb/_search?q=*'+searchQuery.queryString+'*' })
 	//$http({ method: 'GET', url: 'test.json' })
       	.then(function (response) {
@@ -16,4 +40,29 @@ var searchApp = angular.module('searchApp', []);
       	}
    	);
     };
+
+    searchQuery.logURLClick = function($event) {
+      var now = new Date();
+      var logEntry = $event.currentTarget.href + " clicked at " + now.toString();
+      console.log(logEntry);
+      searchQuery.sessionInfo.clicks.push(logEntry);
+      searchQuery.sessionInfo.total_clicks += 1;
+    }
+
+    searchQuery.saveSession = function() {
+      var now = new Date();
+      searchQuery.sessionInfo.end = now.toString();
+      searchQuery.sessionInfo.timeElapsed = now.getTime() - new Date(searchQuery.sessionInfo.start).getTime();
+
+      console.log("Session (sessionID: " + searchQuery.sessionInfo.sessionID +
+      ") ended at " + searchQuery.sessionInfo.end);
+
+      var sessionText = JSON.stringify(searchQuery.sessionInfo);
+      var uri = "data:application/json;charset=UTF-8," + encodeURIComponent(sessionText);
+
+      var a = document.createElement('a');
+      a.href = uri;
+      a.innerHTML = "Right-click and choose 'save as...'";
+      document.body.appendChild(a);
+    }
   }]);
